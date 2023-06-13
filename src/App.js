2666c504c2349f37369b2f86b2f5c02b090eed3a
Davide Giacò <Davide.Giaco@exprivia.com>
2023-06-13 16:01:17 +0200

    sta cazzo di Dana di merda

--- src/App.js
+++ src/App.js
@@ -79,40 +79,40 @@ function App() {
           className="mySwiper max-w-[800px] w-full"
         >
           <SwiperSlide className="w-fit">
-            <Review name="PrinceJ" text="I saw April before heading to Europe and let me tell you she has me typing this message 4000 miles away from Canada.. her pictures don’t do her justice..especially with those juicy plump lips, best bbj experience ive received, treat her good fellas" />
+            <Review name="PrinceJ" text="I saw Dana before heading to Europe and let me tell you she has me typing this message 4000 miles away from Canada.. her pictures don’t do her justice..especially with those juicy plump lips, best bbj experience ive received, treat her good fellas" />
           </SwiperSlide>
           <SwiperSlide>
             <Review name="William72" text="Saw the ad, was skeptical at first with the deposit as I’ve never done this before but glad I followed through, very open minded and you can tell she loves what she does which is rare nowdays" />
           </SwiperSlide>
           <SwiperSlide>
-            <Review name="Bp1241" text="As an avid Diamond agency person April has completely stopped this for me as I became a regular for her, im a very picky person especially when it comes to RP but April is extremely patient and intellectual which was also a big turn on for me" />
+            <Review name="Bp1241" text="As an avid Diamond agency person Dana has completely stopped this for me as I became a regular for her, im a very picky person especially when it comes to RP but Dana is extremely patient and intellectual which was also a big turn on for me" />
           </SwiperSlide>
           <SwiperSlide>
             <Review name="Blueman2" text="Very busy woman but very much worth the wait" />
           </SwiperSlide>
           <SwiperSlide>
-            <Review name="Chipdude69" text="April was amazing!! Did overnights with her and was way more into kink friendly suggestions which is a bonus" />
+            <Review name="Chipdude69" text="Dana was amazing!! Did overnights with her and was way more into kink friendly suggestions which is a bonus" />
           </SwiperSlide>
           <SwiperSlide>
-            <Review name="Canuckslover63" text="I had a session with April at her downtown incall not too long ago, very adorable, petite, with an amazing body and damn, she gives an excellent tease to build up tension. It’s good to know she’s good for the price she charges" />
+            <Review name="Canuckslover63" text="I had a session with Dana at her downtown incall not too long ago, very adorable, petite, with an amazing body and damn, she gives an excellent tease to build up tension. It’s good to know she’s good for the price she charges" />
           </SwiperSlide>
           <SwiperSlide>
             <Review name="Monsterzero" text="Sadly doesn’t offer duos but when she told me it won’t be necessary since handling her is already difficult enough she was not kidding, very legit and knows how to use those lips of hers, bbj was done PFE..highly recommend it" />
           </SwiperSlide>
           <SwiperSlide>
-            <Review name="Rockson" text="Contacted her via email for an online session since I like to build up connection and was 100% worth it. Ended up doing 3 online sessions as I ended up travelling for work but still communicated with April, will be seeing her when I’m back in 1-2 months.. will be writing a follow up review, Thank you, my darling April, I can’t wait" />
+            <Review name="Rockson" text="Contacted her via email for an online session since I like to build up connection and was 100% worth it. Ended up doing 3 online sessions as I ended up travelling for work but still communicated with Dana, will be seeing her when I’m back in 1-2 months.. will be writing a follow up review, Thank you, my darling Dana, I can’t wait" />
           </SwiperSlide>
           <SwiperSlide>
             <Review name="Geniedutch" text="Emailed her to see her availability but wasn’t able to schedule me till a week after since she said she had exams, but after we scheduled she definitely made up for me being patient, highly recommend" />
           </SwiperSlide>
           <SwiperSlide>
-            <Review name="Will2serveU" text="Gentlemen, I just wanted to put in a quick plug for April who is a truly hidden gem …hot session. Reverse cowgirl is forever etched in my memory, unfortunately I am heading back to the States but as she requested for me to leave a review this will not do her any justice. You must experience it for yourself" />
+            <Review name="Will2serveU" text="Gentlemen, I just wanted to put in a quick plug for Dana who is a truly hidden gem …hot session. Reverse cowgirl is forever etched in my memory, unfortunately I am heading back to the States but as she requested for me to leave a review this will not do her any justice. You must experience it for yourself" />
           </SwiperSlide>
           <SwiperSlide>
-            <Review name="GeneralG" text="I reached out after a cancellation, and Miss April did not disappoint. Very personable, friendly, and the skills on point. One of the greatest asses around, and knows how to get to work. Great communication for this meeting, 10/10 would recommend and visit again." />
+            <Review name="GeneralG" text="I reached out after a cancellation, and Miss Dana did not disappoint. Very personable, friendly, and the skills on point. One of the greatest asses around, and knows how to get to work. Great communication for this meeting, 10/10 would recommend and visit again." />
           </SwiperSlide>
           <SwiperSlide>
-            <Review name="LongHorn" text="April immediately made me feel comfortable and at ease with her warm, friendly demeanour. She listened carefully to my concerns and tailored the session to my needs no worries. She asked me to cuddle to warm her up before she started giving me a massage which was pretty damn cute… I couldn’t help but feast my eyes on her absolutely perfect body, this goddess's skills were truly angelic. Beautiful perky breasts with responsive nipples. Nice voluptuous round bum and one of the prettiest and tightest little kitties I have had the pleasure to be inside… I will keep the details to my memories but I would highly recommend her" />
+            <Review name="LongHorn" text="Dana immediately made me feel comfortable and at ease with her warm, friendly demeanour. She listened carefully to my concerns and tailored the session to my needs no worries. She asked me to cuddle to warm her up before she started giving me a massage which was pretty damn cute… I couldn’t help but feast my eyes on her absolutely perfect body, this goddess's skills were truly angelic. Beautiful perky breasts with responsive nipples. Nice voluptuous round bum and one of the prettiest and tightest little kitties I have had the pleasure to be inside… I will keep the details to my memories but I would highly recommend her" />
           </SwiperSlide>
         </Swiper>
       </div>
@@ -121,7 +121,7 @@ function App() {
 
       <div className="w-full h-[80px] flex items-center justify-center bg-lpink px-10 fixed top-0">
         <div className="flex items-center justify-between h-full w-full max-w-[1400px]">
-          <p className="text-2xl font-semibold">April Rose</p>
+          <p className="text-2xl font-semibold">Dana Bella</p>
           {
             menuQuery3 &&
             <>
@@ -166,7 +166,7 @@ function App() {
                   <MoreLink text="Touring dates" closeDropDown={() => setOpenedMore(false)} href="touring" position="center" />
                   <MoreLink text="Rules" closeDropDown={() => setOpenedMore(false)} href="rules" position="center" />
 
-                  {!menuQuery3 && <MoreLink text="Twitter" closeDropDown={() => setOpenedMore(false)} href="https://twitter.com/latinagirl789" />}
+                  {!menuQuery3 && <MoreLink text="Twitter" closeDropDown={() => setOpenedMore(false)} href="https://twitter.com/chilegirl789" />}
                 </div>
               </div>
               :
@@ -178,7 +178,7 @@ function App() {
               </>
           }
 
-          {menuQuery3 && <a href="https://twitter.com/latinagirl789"><BsTwitter color="#000" size={20} className="cursor-pointer" /></a>}
+          {menuQuery3 && <a href="https://twitter.com/chilegirl789"><BsTwitter color="#000" size={20} className="cursor-pointer" /></a>}
         </div>
       </div>
 
--- src/frames/Footer.js
+++ src/frames/Footer.js
@@ -2,8 +2,8 @@ const Footer = () =>{
     return(
         <div className="bg-black w-full flex flex-col items-center py-24 h-fit px-5" id="contact">
             <p className="font-bold text-4xl text-white underline"><i>Contact</i></p>
-            <p className="text-white text-xl mt-4 text-center"><b>Phone number:</b> 647-530-5851 (Text only)</p>
-            <p className="text-white text-xl mt-4 text-center"><b>Email:</b> aprilrose6914@gmail.com</p>
+            <p className="text-white text-xl mt-4 text-center"><b>Phone number:</b> 437-993-8332 (Text only)</p>
+            <p className="text-white text-xl mt-4 text-center"><b>Email:</b> danabella6914@gmail.com</p>
         </div>
     )
 }
